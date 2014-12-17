1214099722a149c2d9825b0f46a059f82a1320e0
Brad Davis <brd@FreeBSD.org>
2014-12-17 14:59:25 -0700

    Initial add
    VMWare is working and VirtualBox should too, but will need to be tested.

---
+++ README.md
@@ -0,0 +1,9 @@
+# FreeBSD Packer template
+
+Creates a vagrant box for FreeBSD 10.1-RELEASE.
+
+Run using `packer build template.json`
+
+Notes:
+
+ * The VM is set to have 1024MB of RAM and a 20GB drive
---
+++ scripts/postinstall.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo 'Running post-install..'
+
+echo 'Patching FreeBSD..'
+freebsd-update fetch install > /dev/null
+
+echo 'Setting up VM Tools..'
+if [ "$PACKER_BUILDER_TYPE" = 'vmware-iso' ]; then
+	env ASSUME_ALWAYS_YES=yes pkg install -y open-vm-tools-nox11
+	echo 'vmware_guest_vmblock_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guest_vmhgfs_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guest_vmmemctl_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guest_vmxnet_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guestd_enable="YES"' >> /etc/rc.conf
+elif [ "$PACKER_BUILDER_TYPE" = 'virtualbox-iso' ]; then
+	env ASSUME_ALWAYS_YES=yes pkg install -y virtualbox-ose-additions
+else
+	echo 'Unknown type of VM, not installing tools..'
+fi
+
+echo
+echo 'Setting up sudo..'
+pkg install -y sudo
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /usr/local/etc/sudoers
+
+echo
+echo 'Setting up the vagrant ssh keys'
+pkg install -y curl
+mkdir ~vagrant/.ssh
+chmod 700 ~vagrant/.ssh
+curl -skL -o ~vagrant/.ssh/authorized_keys http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
+chown -R vagrant ~vagrant/.ssh
+chmod 600 ~/vagrant/.ssh/authorized_keys
+
+echo
+echo 'Changing roots shell back'
+chsh -s csh root
+
+echo
+echo 'Post-install complete.'
