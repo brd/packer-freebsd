9e44ea2c271704264cfed6583915dc83bcd9e636
Brad Davis <brd@FreeBSD.org>
2015-01-08 11:08:18 -0700

    Remove an old commented out command

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -33,13 +33,7 @@ echo
 echo 'Setting up the vagrant ssh keys'
 mkdir ~vagrant/.ssh
 chmod 700 ~vagrant/.ssh
-
-#
-# Network failure cause problems here...
-#pkg install -y curl
-#curl -skL -o ~vagrant/.ssh/authorized_keys http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > ~vagrant/.ssh/authorized_keys
-
 chown -R vagrant ~vagrant/.ssh
 chmod 600 ~vagrant/.ssh/authorized_keys
 
